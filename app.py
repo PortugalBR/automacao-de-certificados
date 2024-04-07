d0d9d30e94c579423ce4b140fe4865017d4e7655
PortugalBR <121282411+PortugalBR@users.noreply.github.com>
2024-04-07 19:52:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-"""
-# Pegar os dados da planilha 
-Tipo nome do curso, nome participante, tipo de participação, data do inicio, data do final, carga horária, data da emissão do certificado
-
-# Transferir os dados da planilha para a imagem do certificado
-"""
 # Pegar os dados da planilha 
 import openpyxl
 from PIL import Image, ImageDraw, ImageFont
